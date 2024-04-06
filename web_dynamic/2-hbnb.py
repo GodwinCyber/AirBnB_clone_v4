7b6fa2de250320262289a509dfd0e7b1a920c7ae
GodwinCyber <godwinchuks032@gmail.com>
2024-04-06 09:03:16 -0700

    API status

---
+++ web_dynamic/2-hbnb.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""0-hbnb.py"""
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from os import environs
+from flask import Flask, render_template
+import uuid
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """close session sqlalchemist"""
+    storage.close()
+
+
+@app.route('/0-hbnb/')
+def hbnb_filters(the_id=None):
+    """replace the existing route to /2-hbnb/"""
+    stateObjs = storage.all('State').values()
+    allState = dict([st.name, st] for st in stateObjs)
+    allAmen = storage.all('Amenity').values()
+    allPlace = storage.all('Place').values()
+    allUser = dict([User.id, "{} {}".format(User.first_name, User.last_name)]
+                 for User in storage.all('User').values())
+    cache_id = (str(uuid.uuid4()))
+    return render_template('2-hbnb.html', states=allState, amens=allAmen,
+                           places=allPlace, users=allUser, cache_id=cache_id)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000', debug=True)
---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  const selectedAmenities = {};
+
+  $('.amenities input[type="checkbox"]').change(function() {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if (this.checked) {
+      selectedAmenities[amenityId] = amenityName;
+    } else {
+      delete selectedAmenities[amenityId];
+    }
+
+    const amenitiesList = Object.values(selectedAmenities).join(', ');
+    $('.amenities h4').text(amenitiesList);
+  });
+
+  // Request to the status API
+  $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
