fbf91af6a410f02275e030dbb7e47a1e5174bd80
GodwinCyber <godwinchuks032@gmail.com>
2024-04-07 16:07:20 -0700

    upadted the api

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -16,11 +16,11 @@ $(document).ready(function() {
   });
 
   // Request to the status API
-  $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function(data) {
     if (data.status === 'OK') {
-      $('#api_status').addClass('available');
+      $('div#api_status').addClass('available');
     } else {
-      $('#api_status').removeClass('available');
+      $('div#api_status').removeClass('available');
     }
   });
 });
