f080e5262cc405a7782670946d11a1273f9cd702
YOUNESSE25 <younessehaouas@gmail.com>
2024-04-07 19:03:56 -0700

    v4

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -22,8 +22,8 @@ def hbnb_filters(the_id=None):
     allState = dict([st.name, st] for st in stateObjs)
     allAmen = storage.all('Amenity').values()
     allPlace = storage.all('Place').values()
-    allUser = dict([User.id, "{} {}".format(User.first_name, User.last_name)]
-                    for User in storage.all('User').values())
+    hox = [User.id, "{} {}".format(User.first_name, User.last_name)]
+    allUser = dict(hox for User in storage.all('User').values())
     cache_id = (str(uuid.uuid4()))
     return render_template('0-hbnb.html', states=allState, amenities=allAmen,
                            places=allPlace, users=allUser, cache_id=cache_id)
